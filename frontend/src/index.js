d202002b8341a44559afc0fa988f8afec0033d4d
Vy Nguyen <vydpnguyen@gmail.com>
2024-03-17 22:17:07 -0700

    Add nav bar styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import "bulma/css/bulma.min.css"
+//import "bulma/css/bulma.min.css"
+import './style.css'
 
 ReactDOM.render(<App />, 
                document.getElementById("root"));
