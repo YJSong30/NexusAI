22f885077d590f7c2c4d4ff8a83b04feb60fc89e
Vy Nguyen <vydpnguyen@gmail.com>
2024-03-22 00:12:55 -0700

    Add images folder

--- frontend/src/MainScreen.jsx
+++ frontend/src/MainScreen.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import Main from './components/Main';
 
 const MainScreen = () => {
   return (
-    <header>
+    <div>
       <NavBar />
-    </header>
+      <Main />
+    </div>
   );
 };
 
