dd123fe4a9475551257c3fedb4cf305b210f674e
Vy Nguyen <vydpnguyen@gmail.com>
2024-03-22 00:10:00 -0700

    Add images and created main page

--- frontend/src/MainScreen.jsx
+++ frontend/src/MainScreen.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import Main from './components/Main';
 
 const MainScreen = () => {
   return (
     <header>
       <NavBar />
+      <Main />
     </header>
   );
 };
---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div class="backdrop-container">
+        <div class="backdrop-content">
+            <h1 class="poppins-bold">A revolution for your game backdrop</h1>
+            <p class="poppins-regular">Convert words into unique 2D backdrops with our custom-trained AI model</p>
+            <button class="backdrop-button poppins-regular">Start Generating</button>
+        </div>
+    </div>
+  );
+};
+
+export default Main;
