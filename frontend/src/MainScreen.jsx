0e95e6da44bb234a0205264644b8e95758f4e780
Vy Nguyen <vydpnguyen@gmail.com>
2024-03-22 10:44:56 -0700

    Created About section

--- frontend/src/MainScreen.jsx
+++ frontend/src/MainScreen.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import Main from './components/Main';
+import About from './components/About'
 
 const MainScreen = () => {
   return (
     <div class='main-screen'>
       <NavBar />
       <Main />
+      <About />
     </div>
   );
 };
---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div class="about-container">
+        <div class="about-content">
+            <h2 class="poppins-bold">Elevate your Game with NexusAI</h2>
+            <p class="poppins-regular">NexusAI offers an innovative AI-powered tool for backdrop creation to elevate your game's aesthetic. Instantly enhance your game's visuals with our easy-to-use platform, designed to accelerate your game development journey.</p>
+        </div>
+        <div class="about-image-container">
+            <img src='https://cdn-uploads.huggingface.co/production/uploads/65dd004596c6e35c81161f4c/o2VV3Fp3xpiN0Inv1M3pN.png'/>
+            <img src='https://cdn-uploads.huggingface.co/production/uploads/65dd004596c6e35c81161f4c/nPyGxIzAixVEtQZUuya2R.png'/>
+            <img src='https://cdn-uploads.huggingface.co/production/uploads/65dd004596c6e35c81161f4c/-esGoFbPjKRLKSIs32SrG.png'/>
+            <img src='https://cdn-uploads.huggingface.co/production/uploads/65dd004596c6e35c81161f4c/pj1Ti05wb5Ymu5jLGUgzf.png'/>
+            <img src='https://cdn-uploads.huggingface.co/production/uploads/65dd004596c6e35c81161f4c/nPyGxIzAixVEtQZUuya2R.png'/>
+            <img src='https://cdn-uploads.huggingface.co/production/uploads/65dd004596c6e35c81161f4c/o2VV3Fp3xpiN0Inv1M3pN.png'/>
+            <img src='https://cdn-uploads.huggingface.co/production/uploads/65dd004596c6e35c81161f4c/nPyGxIzAixVEtQZUuya2R.png'/>
+            <img src='https://cdn-uploads.huggingface.co/production/uploads/65dd004596c6e35c81161f4c/o2VV3Fp3xpiN0Inv1M3pN.png'/>
+        </div>
+    </div>
+  );
+};
+
+export default About;
