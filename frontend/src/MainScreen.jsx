7d82729b01e1d928f1da8d203644aa2d6c0f4e4c
Vy Nguyen <vydpnguyen@gmail.com>
2024-03-29 12:25:53 -0700

    Edit image file. Add footer section.

--- frontend/src/MainScreen.jsx
+++ frontend/src/MainScreen.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import Main from './components/Main';
 import About from './components/About'
-import Tutorial from './components/Tutorial';
+//import Tutorial from './components/Tutorial';
 import FAQ from './components/FAQ';
+import Footer from './components/Footer';
 
 const MainScreen = () => {
   return (
@@ -12,6 +13,7 @@ const MainScreen = () => {
       <Main />
       <About />
       <FAQ />
+      <Footer />
     </div>
   );
 };
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <footer>
+            <div class="copyright-div">
+                <p class="poppins-regular">© NexusAI 2024</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
