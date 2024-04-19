b411fd27640429f96d53dc05cf7d00f1b94fce66
Nick Ahn <nickahn01@gmail.com>
2024-04-19 00:01:40 -0700

    Add logs

--- aws/scripts/validate-service.sh
+++ aws/scripts/validate-service.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # validate-service.sh: Check for status health of docker services after after-instal.sh
+date >> ~/validate.log
 
 # Check if all services are running
 docker-compose ps --filter "status=running" | while read -r service_name; do
     if ! docker-compose ps | grep -q "$service_name.*Up"; then
-        echo "Service $service_name is not running."
+        echo "Service $service_name is not running." >> ~/validate.log
         exit 1
     fi
 done
 
-echo "All services are running successfully."
+echo "All services are running successfully." >> ~/validate.log
 exit 0
