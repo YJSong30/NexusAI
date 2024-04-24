a657a8774a0c672d7a486cb61d39af94aeea580e
youngsong1230@gmail.com <youngsong1230@gmail.com>
2024-04-24 14:29:19 -0700

    reordered questions

--- frontend/src/components/FAQ.jsx
+++ frontend/src/components/FAQ.jsx
@@ -16,40 +16,6 @@ const FAQ = () => {
           life!
         </p>
       </div>
-      <div class="faq-question">
-        <h4 class="poppins-bold">Is NexusAI only for game developers?</h4>
-        <p class="poppins-regular">
-          No, NexusAI is not exclusively for game developers. NexusAI is a
-          versatile tool that can cater to creatives, professionals, and
-          educators alike, providing them with a seamless way to generate images
-          for their projects.
-        </p>
-      </div>
-      <div class="faq-question">
-        <h4 class="poppins-bold">Does the AI create unique images?</h4>
-        <p class="poppins-regular">
-          Yes, NexusAI generates unique images tailored to your needs. However,
-          to unlock its full potential, prompts should include one of our 20
-          pre-defined keywords, such as '2D-cavescene', '2D-townscene', etc.
-          This approach ensures that the AI precisely understands the context
-          and style you're aiming for, resulting in images that are both unique
-          and aligned with your vision. You can also experiment with different
-          keywords to see how the AI interprets them. In addition, you can
-          change the point of view of the scene by using the keywords such as
-          'topview' or 'sideview'. What you put in as your prompt will directly
-          affect the output of the AI, so be specific as possible.
-        </p>
-      </div>
-      <div class="faq-question">
-        <h4 class="poppins-bold">
-          What are the system requirements to use NexusAI?
-        </h4>
-        <p class="poppins-regular">
-          NexusAI is a cloud-based platform, meaning you can access it from any
-          device with an internet connection and a web browser. There are no
-          specific system requirements to use our service.
-        </p>
-      </div>
 
       <div class="faq-question">
         <h4 class="poppins-bold">
@@ -65,6 +31,43 @@ const FAQ = () => {
           looking for and produce results that match your requirements. You can
           find a list of available keywords on our platform to experiment with
           and see how they influence the generated images.
+        </p>
+      </div>
+
+      <div class="faq-question">
+        <h4 class="poppins-bold">Does the AI create unique images?</h4>
+        <p class="poppins-regular">
+          Yes, NexusAI generates unique images tailored to your needs. However,
+          to unlock its full potential, prompts should include one of our 20
+          pre-defined keywords, such as '2D-cavescene', '2D-townscene', etc.
+          This approach ensures that the AI precisely understands the context
+          and style you're aiming for, resulting in images that are both unique
+          and aligned with your vision. You can also experiment with different
+          keywords to see how the AI interprets them. In addition, you can
+          change the point of view of the scene by using the keywords such as
+          'topview' or 'sideview'. What you put in as your prompt will directly
+          affect the output of the AI, so be specific as possible.
+        </p>
+      </div>
+
+      <div class="faq-question">
+        <h4 class="poppins-bold">Is NexusAI only for game developers?</h4>
+        <p class="poppins-regular">
+          No, NexusAI is not exclusively for game developers. NexusAI is a
+          versatile tool that can cater to creatives, professionals, and
+          educators alike, providing them with a seamless way to generate images
+          for their projects.
+        </p>
+      </div>
+
+      <div class="faq-question">
+        <h4 class="poppins-bold">
+          What are the system requirements to use NexusAI?
+        </h4>
+        <p class="poppins-regular">
+          NexusAI is a cloud-based platform, meaning you can access it from any
+          device with an internet connection and a web browser. There are no
+          specific system requirements to use our service.
         </p>
       </div>
     </div>
